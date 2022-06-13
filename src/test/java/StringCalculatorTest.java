66616e918dcaa63f2ad1bcc9e10fe3e2a5268f86
boonku <bak.szymon01@gmail.com>
2022-06-13 19:44:23 +0200

    add step 1

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringCalculatorTest {
 
@@ -7,6 +10,21 @@ class StringCalculatorTest {
     @BeforeAll
     static void initAll() {
         stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    void When_SumOfEmptyString_Expect_Zero() {
+        assertEquals(0, stringCalculator.add(""));
+    }
+
+    @Test
+    void When_SumOfOne_Expect_One() {
+        assertEquals(1, stringCalculator.add("1"));
+    }
+
+    @Test
+    void When_SumOfOneAndTwo_Expect_Three() {
+        assertEquals(3, stringCalculator.add("1,2"));
     }
 
 }
